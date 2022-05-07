e5112599c1df7f8143a04dc88541bcf14ddeb18f
petru <smart__petea@mail.ru>
2022-05-07 21:29:55 +0300

    one byte

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,14 @@ fn encode(reader: &mut dyn io::Read, writer: &mut dyn io::Write) -> std::io::Res
     let mut input_buffer = [0u8];
     let mut processed_bytes: usize = 0;
 
-    let encoding_size = 5;
+    let encoding_size = 3;
 
     let mut output_buffer = [0u8];
     let mut output_buffer_size = 8u8;
     let mut input_buffer_size = 0;
 
     loop {
+        println!("\n");
         println!("input_buffer_size={:?}", input_buffer_size);
         if input_buffer_size == 0 {
             let size = reader.read(&mut input_buffer)?;
@@ -25,10 +26,10 @@ fn encode(reader: &mut dyn io::Read, writer: &mut dyn io::Write) -> std::io::Res
 
         let mask_size = std::cmp::min(input_buffer_size, output_buffer_size);
         println!("mask_size={:?}", mask_size);
-        let mask = ((1 << (mask_size + 1)) - 1);
+        let mask: u8 = (1 << (mask_size)) - 1;
+        println!("mask={:#b}", mask);
         println!("output_buffer={:#b}", output_buffer[0]);
-        output_buffer[0] = output_buffer[0] << mask_size;
-        output_buffer[0] = output_buffer[0] | (input_buffer[0] & mask); 
+        output_buffer[0] = output_buffer[0] | ((input_buffer[0] & mask) << (output_buffer_size - mask_size)); 
         println!("output_buffer={:#b}", output_buffer[0]);
 
         input_buffer_size = input_buffer_size - mask_size;
@@ -58,31 +59,33 @@ mod tests {
 
     #[test]
     fn test_encode_one_byte() {
-        let input_byte: u8 = (1 << 7) | 5u8;
+        let input_byte: u8 = (1 << 7) | 7u8;
         let input = [input_byte];
         let mut output = [0u8; 1];
         let result = encode(&mut &input[..], &mut &mut output[..]);
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), 1);
-        assert_eq!(output[0], 5u8);
+        assert_eq!(output[0], 0b11100000u8);
+
+        let input_byte: u8 = (1 << 7) | 8u8;
+        let input = [input_byte];
+        let mut output = [0u8; 1];
+        let result = encode(&mut &input[..], &mut &mut output[..]);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 1);
+        assert_eq!(output[0], 0b00000000u8);
     }
 
     #[test]
-    fn test_encode_two_bytes() {
-        let o_byte0: u8 = 0b11;
-        let i_byte0: u8 = (1 << 7) | o_byte0;
-
-        let o_byte1: u8 = 0b101;
-        let i_byte1: u8 = (1 << 7) | o_byte1;
-
-        let input = [i_byte0, i_byte1];
+    fn test_encode_three_bytes() {
+        let input = [7u8, 1u8, 2u8];
         let mut output = [0u8; 2]; 
 
         //second output byte is to be checked
         let result = encode(&mut &input[..], &mut &mut output[..]);
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), 2);
-        assert_eq!(output[0], 0b00011001u8);
-        //assert_eq!(output[1], 0b01000000u8); todo
+        assert_eq!(output[0], 0b11100101u8);
+        assert_eq!(output[1], 0b00000000u8); 
     }
 }
