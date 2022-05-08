4a976130efaf02aea8a348aa7b351bce2b82f2ef
petru <smart__petea@mail.ru>
2022-05-08 10:51:39 +0300

    decode 1 byte

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,48 @@
 use std::io;
 
+fn decode(encoding_size: u8, reader: &mut dyn io::Read, writer: &mut dyn io::Write) -> std::io::Result<usize> {
+    let mut input_buffer = [0u8];
+    let mut processed_bytes: usize = 0;
+
+    let mut output_buffer = [0u8];
+    let mut output_buffer_size = encoding_size;
+    let mut input_buffer_size = 0;
+
+    loop {
+        if input_buffer_size == 0 {
+            let size = reader.read(&mut input_buffer)?;
+            if size == 0 {
+                //no more data to read
+                break;
+            }
+
+            processed_bytes += size;
+            input_buffer_size = 8u8;
+        }
+
+        let mask_size = std::cmp::min(output_buffer_size, input_buffer_size);
+        let mask = ((1 << mask_size) - 1) << (8 - mask_size);
+        output_buffer[0] <<= encoding_size - mask_size;
+        output_buffer[0] |= (input_buffer[0] & mask) >> (8 - mask_size);
+        input_buffer[0] <<= mask_size;
+
+        output_buffer_size -= mask_size;
+        input_buffer_size -= mask_size;
+        //println!("mask_size={} mask={:#b} input_buffer={:#b} output_buffer_size={}", mask_size, mask, input_buffer[0], output_buffer_size);
+
+        if output_buffer_size == 0 {
+            writer.write(&mut output_buffer)?;
+            output_buffer_size = encoding_size;
+        }
+    }
+
+    if output_buffer_size > 0 {
+        writer.write(&mut output_buffer)?;
+    }
+
+    Ok(processed_bytes)
+}
+
 fn encode(encoding_size: u8, reader: &mut dyn io::Read, writer: &mut dyn io::Write) -> std::io::Result<usize> {
     let mut input_buffer = [0u8];
     let mut processed_bytes: usize = 0;
@@ -47,7 +90,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_5_length_one_byte() {
+    fn test_encode_5_length_one_byte() {
         let input_byte: u8 = (1 << 7) | 7u8;
         let input = [input_byte];
         let mut output = [0u8; 1];
@@ -66,7 +109,7 @@ mod tests {
     }
 
     #[test]
-    fn test_3_length_one_byte() {
+    fn test_encode_3_length_one_byte() {
         let input_byte: u8 = (1 << 7) | 7u8;
         let input = [input_byte];
         let mut output = [0u8; 1];
@@ -112,5 +155,25 @@ mod tests {
         assert_eq!(output[0], 0b00010101u8);
         assert_eq!(output[1], 0b11101101u8); 
         assert_eq!(output[2], 0b11110000u8); 
+    }
+
+    #[test]
+    fn test_decode_3_length_one_byte() {
+        let input = [0b11100000u8];
+        let mut output = [0u8; 1];
+        let result = decode(3, &mut &input[..], &mut &mut output[..]);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 1);
+        assert_eq!(output[0], 0b00000111u8);
+
+        /*
+        let input_byte: u8 = (1 << 7) | 8u8;
+        let input = [input_byte];
+        let mut output = [0u8; 1];
+        let result = encode(3, &mut &input[..], &mut &mut output[..]);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 1);
+        assert_eq!(output[0], 0b00000000u8);
+        */
     }
 }
